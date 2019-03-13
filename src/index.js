c86a2415c3099ee4cf0c27b6e9e28234f9b8dd0b
pgomesdev <gomes.pedro15@gmail.com>
2019-03-13 19:48:29 -0300

    core: added react-router-dom as a dependency and included BrowserRouter to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { endpoint } from './utils/config'
 import './index.css'
@@ -13,7 +14,9 @@ const client = new ApolloClient({
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ApolloProvider>,
   // eslint-disable-next-line
   document.getElementById('root')
